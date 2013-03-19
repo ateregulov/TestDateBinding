b16210cb805914cd4a85605cb5192fa77e87a493
ateregulov <sliceice@gmail.com>
2013-03-19 11:08:59 +0400

    Get and post form

---
+++ TestDateBinding/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestDateBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(DateTime? date )
+        {
+            return View(date ?? DateTime.Parse("01/01/0001"));
+        }
+
+        [HttpPost]
+        public ActionResult Index(DateTime? date, int? id)
+        {
+            return View(date ?? DateTime.Parse("01/01/0001"));
+        }
+
+        
+    }
+}
